bd79c8df661d1ee8bddf9aac8b2c4f1b85757f75
Yashi Garg <168455181+YashiGarg016@users.noreply.github.com>
2026-04-23 00:06:23 +0530

    Create 135-Candy

---
+++ 135-Candy/135-Candy.java
@@ -0,0 +1,30 @@
+// Last updated: 23/04/2026, 00:06:23
+class Solution {
+    public int candy(int[] ratings) {
+        int sum = 1, i = 1; 
+        int n = ratings.length;
+        while(i < n){
+            if(ratings[i] == ratings[i - 1]){
+                sum += 1;
+                i++;
+                continue;
+            }
+
+            int peak = 1;
+            while(i < n && ratings[i] > ratings[i - 1]){
+                peak += 1;
+                sum += peak;
+                i++;
+            }
+
+            int down = 1;
+            while(i < n && ratings[i] < ratings[i - 1]){
+                sum += down;
+                i++;
+                down++;
+            }
+            if(down > peak) {sum += down - peak;}
+        }
+        return sum;
+    }
+}
