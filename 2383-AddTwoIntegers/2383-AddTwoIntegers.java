29ce682fe505a3810b19f254bf5c352b70dd14eb
Yashi Garg <168455181+YashiGarg016@users.noreply.github.com>
2026-04-23 00:02:43 +0530

    Create 2383-AddTwoIntegers

---
+++ 2383-AddTwoIntegers/2383-AddTwoIntegers.java
@@ -0,0 +1,9 @@
+// Last updated: 23/04/2026, 00:02:42
+class Solution {
+    public int sum(int num1, int num2) {
+        int result;
+        result= num1+ num2;
+        return result;
+        
+    }
+}
