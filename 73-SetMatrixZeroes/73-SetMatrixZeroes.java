bcb6cc141e826b2bd19b86f892b529a67bb8d5e4
Yashi Garg <168455181+YashiGarg016@users.noreply.github.com>
2026-04-23 00:06:50 +0530

    Create 73-SetMatrixZeroes

---
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.java
@@ -0,0 +1,55 @@
+// Last updated: 23/04/2026, 00:06:50
+class Solution {
+    public void setZeroes(int[][] matrix) {
+    int m = matrix.length;
+    int n = matrix[0].length;
+    boolean fillFirstRow = false;
+    boolean fillFirstCol = false;
+
+    for(int j = 0; j<n; ++j){
+        if(matrix[0][j] == 0){
+            fillFirstRow = true;
+            break;
+        }
+    }
+
+    for(int i = 0; i<m; ++i){
+        if(matrix[i][0] == 0){
+            fillFirstCol = true;
+            break;
+        }
+    }
+
+    for(int i = 1; i<m; ++i){
+        for(int j = 1; j<n; ++j){
+            if(matrix[i][j] == 0){
+                matrix[0][j] = 0;
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    for(int i = 1; i<m; ++i){
+        for(int j = 1; j<n; ++j){
+            if(matrix[i][0] == 0 || matrix[0][j]==0){
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    if(fillFirstRow){
+        for(int j = 0; j<n; ++j){
+            matrix[0][j] = 0;
+        }
+    }
+
+    if(fillFirstCol){
+        for(int i = 0; i<m; ++i){
+            matrix[i][0] = 0;
+        }
+    }
+    
+    
+    }
+
+} 
