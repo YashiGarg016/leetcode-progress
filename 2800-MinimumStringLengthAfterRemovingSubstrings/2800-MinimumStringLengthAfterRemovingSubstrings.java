1b81da7721739b7c938573d962be27a0f862220a
Yashi Garg <168455181+YashiGarg016@users.noreply.github.com>
2026-04-23 00:02:17 +0530

    Create 2800-MinimumStringLengthAfterRemovingSubstrings

---
+++ 2800-MinimumStringLengthAfterRemovingSubstrings/2800-MinimumStringLengthAfterRemovingSubstrings.java
@@ -0,0 +1,20 @@
+// Last updated: 23/04/2026, 00:02:16
+class Solution {
+  public int minLength(String s) {
+    Deque<Character> stack = new ArrayDeque<>();
+
+    for (final char c : s.toCharArray())
+      if (c == 'B' && match(stack, 'A'))
+        stack.pop();
+      else if (c == 'D' && match(stack, 'C'))
+        stack.pop();
+      else
+        stack.push(c);
+
+    return stack.size();
+  }
+
+  private boolean match(Deque<Character> stack, char c) {
+    return !stack.isEmpty() && stack.peek() == c;
+  }
+}
