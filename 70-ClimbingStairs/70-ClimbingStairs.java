26ac5e4245f864ba66fe8dd962f450f6e0d88ccb
Yashi Garg <168455181+YashiGarg016@users.noreply.github.com>
2026-04-23 00:06:52 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,15 @@
+// Last updated: 23/04/2026, 00:06:51
+class Solution {
+    public int climbStairs(int n) {
+        if(n == 0 || n == 1) return 1;
+
+        int dp[] = new int[n+1];
+        dp[0] = dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i -1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+}
