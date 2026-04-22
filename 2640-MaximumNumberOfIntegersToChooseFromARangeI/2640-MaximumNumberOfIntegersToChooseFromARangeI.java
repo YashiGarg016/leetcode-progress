169bebfbcbc644fbffe1207ea4c2f2f70ca91727
Yashi Garg <168455181+YashiGarg016@users.noreply.github.com>
2026-04-23 00:02:27 +0530

    Create 2640-MaximumNumberOfIntegersToChooseFromARangeI

---
+++ 2640-MaximumNumberOfIntegersToChooseFromARangeI/2640-MaximumNumberOfIntegersToChooseFromARangeI.java
@@ -0,0 +1,16 @@
+// Last updated: 23/04/2026, 00:02:27
+class Solution {
+  public int maxCount(int[] banned, int n, int maxSum) {
+    int ans = 0;
+    int sum = 0;
+    Set<Integer> bannedSet = Arrays.stream(banned).boxed().collect(Collectors.toSet());
+
+    for (int i = 1; i <= n; ++i)
+      if (!bannedSet.contains(i) && sum + i <= maxSum) {
+        ++ans;
+        sum += i;
+      }
+
+    return ans;
+  }
+}
