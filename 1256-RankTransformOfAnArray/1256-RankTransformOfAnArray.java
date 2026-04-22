9e36ab24a5cf3d99679f14b790b77ab56d3ff647
Yashi Garg <168455181+YashiGarg016@users.noreply.github.com>
2026-04-23 00:03:50 +0530

    Create 1256-RankTransformOfAnArray

---
+++ 1256-RankTransformOfAnArray/1256-RankTransformOfAnArray.java
@@ -0,0 +1,17 @@
+// Last updated: 23/04/2026, 00:03:49
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] sortedArr = arr.clone();
+    Map<Integer, Integer> rank = new HashMap<>();
+
+    Arrays.sort(sortedArr);
+
+    for (final int a : sortedArr)
+      rank.putIfAbsent(a, rank.size() + 1);
+
+    for (int i = 0; i < arr.length; ++i)
+      arr[i] = rank.get(arr[i]);
+
+    return arr;
+    }
+}
