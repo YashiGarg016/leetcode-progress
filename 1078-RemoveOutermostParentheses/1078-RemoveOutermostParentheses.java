e5fff6d455be2244d0383414937e3481c7eec937
Yashi Garg <168455181+YashiGarg016@users.noreply.github.com>
2026-04-23 00:04:00 +0530

    Create 1078-RemoveOutermostParentheses

---
+++ 1078-RemoveOutermostParentheses/1078-RemoveOutermostParentheses.java
@@ -0,0 +1,22 @@
+// Last updated: 23/04/2026, 00:04:00
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int len = s.length();
+        if (len <= 2) return "";
+        char[] c = s.toCharArray();
+        StringBuilder newString = new StringBuilder();
+        int open = 1;
+        int openLeft = 0;
+        for (int i = 1; i < len; i++) {
+            if (c[i] == '(') {
+                open++;
+                if (open > 1) newString.append('(');
+            }
+            else {
+                if (open > 1) newString.append(')');
+                open--;
+            }
+        }
+        return newString.toString();
+    }
+}
