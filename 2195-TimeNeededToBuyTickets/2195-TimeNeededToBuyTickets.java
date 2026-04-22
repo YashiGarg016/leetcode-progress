c7909f2fb14130c5748b1f3ac8177f30a9f88722
Yashi Garg <168455181+YashiGarg016@users.noreply.github.com>
2026-04-23 00:02:48 +0530

    Create 2195-TimeNeededToBuyTickets

---
+++ 2195-TimeNeededToBuyTickets/2195-TimeNeededToBuyTickets.java
@@ -0,0 +1,16 @@
+// Last updated: 23/04/2026, 00:02:47
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int total = 0;
+
+        for(int i = 0; i < tickets.length; i++){
+            if(i <= k){
+                total += Math.min(tickets[i], tickets[k]);
+            }else{
+                total += Math.min(tickets[i], tickets[k] - 1);
+            }
+        }
+
+        return total;
+    }
+}
