0b0733b9a1505342352dd7c45e3182091ab653a8
Yashi Garg <168455181+YashiGarg016@users.noreply.github.com>
2026-04-23 00:02:42 +0530

    Create 2456-ConstructSmallestNumberFromDiString

---
+++ 2456-ConstructSmallestNumberFromDiString/2456-ConstructSmallestNumberFromDiString.cpp
@@ -0,0 +1,23 @@
+// Last updated: 23/04/2026, 00:02:41
+class Solution {
+ public:
+  string smallestNumber(string pattern) {
+    string ans;
+    stack<char> stack{{'1'}};
+
+    for (const char c : pattern) {
+      char maxSorFar = stack.top();
+      if (c == 'I')
+        while (!stack.empty()) {
+          maxSorFar = max(maxSorFar, stack.top());
+          ans += stack.top(), stack.pop();
+        }
+      stack.push(maxSorFar + 1);
+    }
+
+    while (!stack.empty())
+      ans += stack.top(), stack.pop();
+
+    return ans;
+  }
+};
