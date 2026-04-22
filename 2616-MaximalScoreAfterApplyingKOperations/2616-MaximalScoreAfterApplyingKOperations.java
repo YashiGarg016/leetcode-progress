3ee381ce88f6f317554464a857f75d791d38fe7c
Yashi Garg <168455181+YashiGarg016@users.noreply.github.com>
2026-04-23 00:02:29 +0530

    Create 2616-MaximalScoreAfterApplyingKOperations

---
+++ 2616-MaximalScoreAfterApplyingKOperations/2616-MaximalScoreAfterApplyingKOperations.java
@@ -0,0 +1,18 @@
+// Last updated: 23/04/2026, 00:02:28
+class Solution {
+  public long maxKelements(int[] nums, int k) {
+    long ans = 0;
+    Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+    for (final int num : nums)
+      maxHeap.offer(num);
+
+    for (int i = 0; i < k; ++i) {
+      final int num = maxHeap.poll();
+      ans += num;
+      maxHeap.offer((num + 2) / 3);
+    }
+
+    return ans;
+  }
+}
