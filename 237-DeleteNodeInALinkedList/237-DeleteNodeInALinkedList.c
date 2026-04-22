b9ed0931b1d1ac1dd82a6bf2a33366e25e199015
Yashi Garg <168455181+YashiGarg016@users.noreply.github.com>
2026-04-23 00:05:31 +0530

    Create 237-DeleteNodeInALinkedList

---
+++ 237-DeleteNodeInALinkedList/237-DeleteNodeInALinkedList.c
@@ -0,0 +1,12 @@
+// Last updated: 23/04/2026, 00:05:31
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+void deleteNode(struct ListNode* node) {
+    node->val = node->next->val;
+    node->next = node->next->next;
+}
